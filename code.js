92240886eb5ed84af0062d7365f2b75af63ba4d5
Maikeru <63614642+maikeru-dev@users.noreply.github.com>
2022-02-08 22:49:18 +0000

    auto wallpaper change
    this updates includes the function where the code by default changes the wallpaper every 30 minutes, also requests for a new set when it runs out

--- code.js
+++ code.js
@@ -2,31 +2,68 @@
 let welcomes = ['Good afternoon,','Good morning,']
 let username = 'Michael'
 var nowTime = new Date();
+var lastbackgroundTime = new Date();
 let tick = false;
 let photos = []
 let apikey = "Bearer 563492ad6f917000010000011c2607a74b374f9f8daf3428ddc31151"
+document.addEventListener("mousemove", parallax);
 function changetext(id1, shadowid, text){document.getElementById(id1).innerText = text;document.getElementById(shadowid).innerText = text;}
 function semicolonchange(text){document.getElementById('semicolon').style.visibility = text;document.getElementById('semicolonshadow').style.visibility = text}
 function timechange(hours,minutes){changetext('hours','hoursshadow', hours);changetext('minutes','minutesshadow', minutes)}
 function background(text){document.getElementById('background-image').style.backgroundImage = `url('${text}')`;}
 
 var request = new XMLHttpRequest();
-request.open('GET','https://api.pexels.com/v1/search?query=nature&orientation=landscape&size=medium')
-request.setRequestHeader('Authorization', apikey)
-request.send();
-request.onload = ()=>{
-    photos = JSON.parse(request.response)['photos']
-    let random = Math.round(Math.random() * photos.length)
-    let data = photos.slice(random-1,random)[0]
-    console.log(data, random)
 
+requestImages('query=nature&orientation=landscape&size=medium')
+checkTime()
+animateclock()
+setInterval(() => {
+    animateclock()
+    if ((nowTime.getTime() - lastbackgroundTime.getTime()) > 1800000){
+        lastbackgroundTime = nowTime
+        loadBackground()
+    }
+
+}, 1000);
+function animateclock(){
+    
+    if(tick == false){
+        semicolonchange('visible')
+        tick = true
+    }else {
+        semicolonchange('hidden')
+        tick = false
+    }
+
+    nowTime = new Date();
+    let hours = nowTime.getHours()
+    let minutes = nowTime.getMinutes()
+    
+    timechange(formatTime(hours),formatTime(minutes))
+}
+function loadBackground(){
+    let random = Math.round(Math.random() * (photos.length-1))
+    let data = photos.splice(random,1)[0]
+    console.log(data, random)
+    if (photos.length < 1){
+        requestImages('query=nature&orientation=landscape&size=medium')
+    }
     let image = data.src['original']
     background(image);
+    changetext('credit','creditshadow',data.photographer)
+}
+function requestImages(query){
+    request.open('GET','https://api.pexels.com/v1/search?' + query)
+    request.setRequestHeader('Authorization', apikey)
+    request.send();
+    request.onload = ()=>{
+        photos = JSON.parse(request.response)['photos']
+        loadBackground()
+    }
 }
 (function () {
     // Add event listener
     document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#background-image");
     
     // Magic happens here, kudos to this guy https://codepen.io/oscicen/pen/zyJeJw
     function parallax(e) {
@@ -44,32 +81,7 @@ request.onload = ()=>{
         document.querySelector("#background-image").style.transform = imagemove;
         document.querySelector("#container").style.transform = textmove;
     }
-
 })();
-checkTime()
-animateclock()
-setInterval(() => {
-    animateclock()
-}, 1000);
-function animateclock(){
-    
-    if(tick == false){
-        semicolonchange('visible')
-        tick = true
-    }else {
-        semicolonchange('hidden')
-        tick = false
-    }
-
-    nowTime = new Date();
-    let hours = nowTime.getHours()
-    let minutes = nowTime.getMinutes()
-    
-    timechange(formatTime(hours),formatTime(minutes))
-    
-}
-document.addEventListener("mousemove", parallax);
-
 function parallax(e){
     this.querySelectorAll('.layer').forEach(layer => {
         const speed = layer.getAttribute('data-speed')
