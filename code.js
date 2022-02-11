e6f0bcd09f2c5c852ece96be6d54d0708d3fc169
Maikeru <63614642+maikeru-dev@users.noreply.github.com>
2022-02-11 01:38:05 +0000

    Full animation upgrade
    the animation was kicked up a notch :)

--- code.js
+++ code.js
@@ -1,65 +1,72 @@
-
-let welcomes = ['Good afternoon,','Good morning,']
-let username = 'Kratomasy'
+let welcomes = ['Good afternoon,','Good morning,'];
+let username = 'Michael';
 var nowTime = new Date();
 var lastbackgroundTime = new Date();
 let tick = false;
-let photos = []
-let apikey = "Bearer 563492ad6f917000010000011c2607a74b374f9f8daf3428ddc31151"
-document.addEventListener("mousemove", parallax);
-function changetext(id1, shadowid, text){document.getElementById(id1).innerText = text;document.getElementById(shadowid).innerText = text;}
-function semicolonchange(text){document.getElementById('semicolon').style.visibility = text;document.getElementById('semicolonshadow').style.visibility = text}
-function timechange(hours,minutes){changetext('hours','hoursshadow', hours);changetext('minutes','minutesshadow', minutes)}
-function background(text){document.getElementById('background-image').style.backgroundImage = `url('${text}')`;}
+let photos = [];
+let apikey = "Bearer 563492ad6f917000010000011c2607a74b374f9f8daf3428ddc31151";
+function changeStyle(id, attribute, text){document.getElementById(id).style[attribute] = text };
+function changeText(id1, shadowid, text){document.getElementById(id1).innerText = text;document.getElementById(shadowid).innerText = text;}
+function semicolonChange(text){changeStyle('semicolon', 'visibility', text);changeStyle('semicolonshadow','visibility', text);}
+function timeChange(hours,minutes){changeText('hours','hoursshadow', hours);changeText('minutes','minutesshadow', minutes);}
 
 var request = new XMLHttpRequest();
 
-requestImages('query=nature&orientation=landscape&size=medium')
-checkTime()
-animateclock()
+requestImages('query=nature&orientation=landscape&size=medium');
+checkTime();
+animateClock();
 setInterval(() => {
-    animateclock()
+    animateClock()
     if ((nowTime.getTime() - lastbackgroundTime.getTime()) > 1800000){
-        lastbackgroundTime = nowTime
-        loadBackground()
-    }
+        lastbackgroundTime = nowTime;
+        loadBackground();
+    };
 
 }, 1000);
-function animateclock(){
-    
+function animateClock(){
     if(tick == false){
-        semicolonchange('visible')
-        tick = true
+        semicolonChange('visible');
+        tick = true;
     }else {
-        semicolonchange('hidden')
-        tick = false
+        semicolonChange('hidden');
+        tick = false;
     }
 
     nowTime = new Date();
-    let hours = nowTime.getHours()
-    let minutes = nowTime.getMinutes()
+    let hours = nowTime.getHours();
+    let minutes = nowTime.getMinutes();
     
-    timechange(formatTime(hours),formatTime(minutes))
+    timeChange(formatTime(hours),formatTime(minutes));
 }
 function loadBackground(){
-    let random = Math.round(Math.random() * (photos.length-1))
-    let data = photos.splice(random,1)[0]
-    console.log(data, random)
+    let data = photos.shift();
+    console.log(photos)
     if (photos.length < 1){
-        requestImages('query=nature&orientation=landscape&size=medium')
-    }
-    let image = data.src['original']
-    background(image);
-    changetext('credit','creditshadow',data.photographer)
+        requestImages('query=nature&orientation=landscape&size=medium') 
+        // we requested before but we use this to make sure we have more images to use
+    };
+
+    let imageUrl = data.src.original; //api path
+    document.getElementById('background-image').src = imageUrl;//unique, doesn't need a function
+    changeText('credit','creditshadow',data.photographer);
+    
+}
+document.getElementById('background-image').onload = ()=> { //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+    let ui = document.getElementsByClassName('ui') // short hand
+    console.log()
+    for(i = 2;i>=0;i--){
+        ui[i].classList.add('ui-block'); // relic, this is beautiful. applys a second, higher priority class to another class
+    };
 }
 function requestImages(query){
-    request.open('GET','https://api.pexels.com/v1/search?' + query)
-    request.setRequestHeader('Authorization', apikey)
+    request.open('GET','https://api.pexels.com/v1/search?' + query);
+    request.setRequestHeader('Authorization', apikey);
     request.send();
     request.onload = ()=>{
-        photos = JSON.parse(request.response)['photos']
-        loadBackground()
-    }
+        photos = JSON.parse(request.response).photos;
+        loadBackground();
+    };
 }
 (function () {
     // Add event listener
@@ -76,36 +83,26 @@ function requestImages(query){
         let ytranvalue = (((_mouseY) - (_h)) * 0.5 / 100)-100;
 
         let imagemove = `translateX(${xtranvalue}px) translateY(${ytranvalue}px)`;
-        let textmove = `translateX(${-(xtranvalue+100)}px) translateY(${-(ytranvalue+100)}px)`
+        let textmove = `translateX(${-(xtranvalue+100)}px) translateY(${-(ytranvalue+100)}px)`;
         
         document.querySelector("#background-image").style.transform = imagemove;
         document.querySelector("#container").style.transform = textmove;
     }
 })();
-function parallax(e){
-    this.querySelectorAll('.layer').forEach(layer => {
-        const speed = layer.getAttribute('data-speed')
-
-        const x = (window.innerWidth - e.pageX*speed)/100
-        const y = (window.innerHeight - e.pageY*speed)/100
-
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
-}
 function formatTime(num){
     if(num >= 10){
-        return num
+        return num;
     }else {
-        return '0'+num
-    }
-}
+        return '0'+num;
+    };
+};
 function checkTime(){
     nowTime = new Date();
     if(nowTime.getHours() >= 12){
         //afternoon
-        changetext('maintext', 'maintextshadow', welcomes[0]+' '+username)
+        changeText('maintext', 'maintextshadow', welcomes[0]+' '+username);
     } else {
         //morning
-        changetext('maintext', 'maintextshadow', welcomes[1]+' '+username)
-    }
-}
+        changeText('maintext', 'maintextshadow', welcomes[1]+' '+username);
+    };
+};
